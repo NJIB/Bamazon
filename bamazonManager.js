6e37a6f53868f1db6d2809f7ea50141387925943
Nicholas Buck <njibuck@gmail.com>
2019-10-31 00:18:58 -0400

    Rolled back Inq function (Customer Manager toggle)

--- bamazonManager.js
+++ bamazonManager.js
@@ -2,19 +2,20 @@ const inquirer = require('inquirer');
 
 /**
  */
-class StockList {
-  /**
-     * @param {Object} dbConn db connection object
-     */
-  constructor(dbConn) {
-    this.dbConn = dbConn;
-  }
+class InventoryList {
+    /**
+       * @param {Object} dbConn db connection object
+       */
+    constructor(dbConn) {
+        this.dbConn = dbConn;
+    }
 
-  /**
-   *
-   */
-  async inventoryItems() {
-    await this.dbConn.getAllItems();
+    /**
+     *
+     */
+    async inventoryItems() {
+        await this.dbConn.getAllItems();
+    }
 }
 
-module.exports = StockList;
+module.exports = InventoryList;
